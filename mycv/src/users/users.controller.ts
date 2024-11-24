bbe993adf5542e3b94ba25e2ca8360f3da0a6b55
MAGHC <windowsapple95@gmail.com>
2024-11-24 17:07:20 +0900

    refactor

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -14,7 +14,7 @@ import {
 import { CreateUserDto } from './dtos/create-user';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user-dto';
-import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
@@ -28,7 +28,7 @@ export class UsersController {
     this.userService.create(body.email, body.password);
   }
 
-  @UseInterceptors(new SerializeInterceptor(UserDto))
+  @Serialize(UserDto)
   @Get('/:id') //db에서는 number지만 요청 받을때는 다 문자열
   async findUser(@Param('id') id: string) {
     console.log('handler is running');
