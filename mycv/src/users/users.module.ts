b30d4e7f3a41828a7edef9cc8ccfe996c5229e5c
MAGHC <windowsapple95@gmail.com>
2024-11-25 16:47:38 +0900

    인증,di복습

--- mycv/src/users/users.module.ts
+++ mycv/src/users/users.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])], //이단계에서 리포지토리 자동 생성
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, AuthService],
 })
 export class UsersModule {}
