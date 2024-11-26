4c0f333d2aa069b084565e99089a808d33db3a44
MAGHC <windowsapple95@gmail.com>
2024-11-26 17:40:18 +0900

    create signin singup

--- mycv/src/users/auth.service.ts
+++ mycv/src/users/auth.service.ts
@@ -1,7 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Injectable()
 export class AuthService {
   constructor(private userService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    // see if email is in use
+    const users = await this.userService.find(email);
+    if (users.length) {
+      throw new BadRequestException('email in use');
+    }
+    // hash the password
+    // create a new user and save it
+    // return the user
+  }
+  signin() {
+    // collect email
+  }
 }
