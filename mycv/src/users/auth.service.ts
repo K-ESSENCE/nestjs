d2b12097a18f15bb0183d7d4bb9ad0f77312ccbc
MAGHC <windowsapple95@gmail.com>
2024-11-27 19:49:19 +0900

    salt and hash

--- mycv/src/users/auth.service.ts
+++ mycv/src/users/auth.service.ts
@@ -1,5 +1,10 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt); // callback대신 promise를 반환하는 함수로 변환
+//typescript에서는 타입을 못잡는경우가있음 그래서 이따가해줄거임
 
 @Injectable()
 export class AuthService {
@@ -12,8 +17,19 @@ export class AuthService {
       throw new BadRequestException('email in use');
     }
     // hash the password
+    //generate a salt
+    const salt = randomBytes(8).toString('hex'); //16
+
+    // hash the salt and the password together
+    const hash = (await scrypt(password, salt, 32)) as Buffer; //32
+
+    // join the hased result and the salt together
+    const result = salt + '.' + hash.toString('hex');
+
     // create a new user and save it
+    const user = await this.userService.create(email, result);
     // return the user
+    return user
   }
   signin() {
     // collect email
--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -14,17 +14,22 @@ import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user-dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private authService: AuthService,
+  ) {}
 
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     //내부 파일 보려면 DTO => DTO로 검증
     console.log(body);
-    this.userService.create(body.email, body.password);
+    // this.userService.create(body.email, body.password);
+    return this.authService.signup(body.email, body.password);
   }
 
   @Get('/:id') //db에서는 number지만 요청 받을때는 다 문자열
