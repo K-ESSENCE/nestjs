a7d9f9fa234dd25d34b5439069e746df681df4ce
MAGHC <windowsapple95@gmail.com>
2024-11-28 17:25:31 +0900

    feat: 쿠키 세션 및 유효성 검증 파이프 설정

--- mycv/src/main.ts
+++ mycv/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+// import cookieSession from 'cookie-session';
+//호환성 문제가있어서 normal import 못함
+const cookieSession = require('cookie-session');
+// nest랑 cookie-session 호환성 문제가있어서 normal import 못함
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['anything'], // 암호화키
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
