74aa9b4e0d9d1f6d18a6a40a2c0945499d2af7aa
MAGHC <windowsapple95@gmail.com>
2024-12-01 16:08:22 +0900

    set global interceptors

--- mycv/src/users/users.module.ts
+++ mycv/src/users/users.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
@@ -9,6 +10,13 @@ import { CurrentUserInterceptor } from './interceptors/current-user.interceptor'
 @Module({
   imports: [TypeOrmModule.forFeature([User])], //이단계에서 리포지토리 자동 생성
   controllers: [UsersController],
-  providers: [UsersService, AuthService, CurrentUserInterceptor],
+  providers: [
+    UsersService,
+    AuthService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CurrentUserInterceptor,
+    }, // 이렇게 하면 모든 요청에 대해서 인터셉터가 실행됨
+  ],
 })
 export class UsersModule {}
