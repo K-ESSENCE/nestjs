413dca9d163516b4f5fa1eef1128a49fd3bf7e63
MAGHC <windowsapple95@gmail.com>
2024-12-02 15:06:50 +0900

    refactor

--- mycv/src/users/auth.service.spec.ts
+++ mycv/src/users/auth.service.spec.ts
@@ -3,30 +3,36 @@ import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 
-it('can create an instance of auth service', async () => {
-  // 가짜 서비스
+describe('AuthService', () => {
+  let service: AuthService;
 
-  const fakeUserService: Partial<UsersService> = {
-    //이객체가 UsersService 타입이지만 일부만 구현된 객체
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password } as User),
-  };
+  beforeEach(async () => {
+    // 가짜 서비스
 
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      {
-        provide: UsersService,
-        useValue: fakeUserService,
-        // di 서비스를 살짝 속이는 작업 누가 UserService를 사용하려고한다고하면 fakeValue를 쓰겠다
-        // 이렇게 하면 테스트 코드에서 원래 서비스를 사용하는게 아니라 가짜 서비스를 사용하게됨
-        // sqlLIte안써도된다
-      },
-    ],
-  }).compile();
+    const fakeUserService: Partial<UsersService> = {
+      //이객체가 UsersService 타입이지만 일부만 구현된 객체
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+    };
 
-  const service = module.get(AuthService);
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUserService,
+          // di 서비스를 살짝 속이는 작업 누가 UserService를 사용하려고한다고하면 fakeValue를 쓰겠다
+          // 이렇게 하면 테스트 코드에서 원래 서비스를 사용하는게 아니라 가짜 서비스를 사용하게됨
+          // sqlLIte안써도된다
+        },
+      ],
+    }).compile();
 
-  expect(service).toBeDefined(); // 서비스가 정의되어있는지 확인
+    service = module.get(AuthService);
+  });
+
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined(); // 서비스가 정의되어있는지 확인
+  });
 });
