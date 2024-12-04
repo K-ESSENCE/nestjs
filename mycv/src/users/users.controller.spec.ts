4a5e26c9993ecd6d06228f5df1b35a8cf55634e1
MAGHC <windowsapple95@gmail.com>
2024-12-04 17:31:28 +0900

    test

--- mycv/src/users/users.controller.spec.ts
+++ mycv/src/users/users.controller.spec.ts
@@ -14,13 +14,14 @@ describe('UsersController', () => {
     fakeUserService = {
       findOne: (id: number) =>
         Promise.resolve({ id, email: 'asdf@asdf.com' } as User),
-      find: (email: string) => Promise.resolve([]),
+      find: (email: string) => Promise.resolve([{ id: 1, email } as User]),
       // remove: (id: number) => Promise.resolve({} as User),
       // update: (id: number) => Promise.resolve({} as User),
     };
     fakeAuthService = {
       // signup: (email: string, password: string) => Promise.resolve({} as User),
-      // signin: (email: string, password: string) => Promise.resolve({} as User),
+      signin: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email } as User),
     };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
@@ -42,4 +43,33 @@ describe('UsersController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('findAllUsers returns a list of users with the given email', async () => {
+    const users = await controller.findAllUsers('asdf@asdf.com');
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('asdf@asdf.com');
+  });
+
+  it('findUser returns a single user with the given id', async () => {
+    const user = await controller.findUser('1');
+    expect(user).toBeDefined();
+  });
+
+  it('findUser throws an error if user with given id is not found', async () => {
+    fakeUserService.findOne = () => Promise.resolve(null);
+    await expect(controller.findUser('1')).rejects.toThrow(NotFoundException);
+  });
+
+  it('signin updates session object and returns user', async () => {
+    const session = { userId: -10 };
+    const user = await controller.signin(
+      { email: 'asdf@asdf.com', password: 'asdf' },
+      session,
+    );
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
+  });
 });
+
+// 데코레이터 테스트는 e2e 테스트를해야함
+// 컨트롤러에 로직이 많으면 안됨
