aea5d6ea17e393fcc8d78ced5342ceeba5c3024e
MAGHC <windowsapple95@gmail.com>
2024-12-15 17:28:52 +0900

    comment 테스트 중복 처리하는법

--- mycv/src/app.module.ts
+++ mycv/src/app.module.ts
@@ -11,7 +11,7 @@ import { Reports } from 'src/reports/reports.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
-      database: 'db.sqlite',
+      database: 'db.sqlite', //이걸로 실행될게 바뀌기때문에 이걸로 테스트모드 선정하면됨 => 환경변수 활용
       entities: [User, Reports], //앱안에저장하려는거 전부 나열
       synchronize: true,
     }),
