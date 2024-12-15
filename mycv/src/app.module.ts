9c4aefe12115864433baf397869e6a2c967a34c6
MAGHC <windowsapple95@gmail.com>
2024-12-15 17:49:27 +0900

    env setting

--- mycv/src/app.module.ts
+++ mycv/src/app.module.ts
@@ -4,17 +4,34 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { User } from './users/user.entity';
 import { Reports } from 'src/reports/reports.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite', //이걸로 실행될게 바뀌기때문에 이걸로 테스트모드 선정하면됨 => 환경변수 활용
-      entities: [User, Reports], //앱안에저장하려는거 전부 나열
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}.local`,
     }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService], // typeorm Module 설정동안 ConfigService의 인스턴스에 엑세스한다는 의미
+      useFactory: (config: ConfigService) => {
+        // di
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          synchronize: true,
+          entities: [User, Reports],
+        };
+      },
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   database: 'db.sqlite', //이걸로 실행될게 바뀌기때문에 이걸로 테스트모드 선정하면됨 => 환경변수 활용
+    //   entities: [User, Reports], //앱안에저장하려는거 전부 나열
+    //   synchronize: true,
+    // }),
     UsersModule,
     ReportsModule,
   ],
