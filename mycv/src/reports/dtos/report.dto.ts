88f3ca5ff3d9139e37bfb5e731942ef69e45b2a7
MAGHC <windowsapple95@gmail.com>
2025-02-02 15:53:18 +0900

    feat,fix: 필요한 정보만 노출되도록 변경

---
+++ mycv/src/reports/dtos/report.dto.ts
@@ -0,0 +1,25 @@
+
+import{ Expose,Transform} from 'class-transformer';
+import {User} from '../../users/user.entity';
+
+export class ReportDto{
+    @Expose()
+    id:number;
+    @Expose()
+    price:number;
+    @Expose()
+    year:number;
+    @Expose()
+    lng:number;
+    @Expose()
+    lat:number;
+    @Expose()
+    mileage:number;
+    @Expose()
+    make:string;
+    @Expose()
+    model:string;
+    @Expose()
+    @Transform(({obj})=>obj.user.id)
+    userId:number
+}
